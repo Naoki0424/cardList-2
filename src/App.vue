307a7db8de5ef08819c8c9774e62c6389c5b37b8
Naoki Tsunoda <Tn04240709@icloud.com>
2020-11-25 10:39:51 +0900

    なんか変更が発生したからコミット

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,28 @@
 <template>
   <div id="app">
-    <container></container>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import container from './components/container.vue'
+import HelloWorld from './components/HelloWorld.vue'
+
 export default {
-  name: 'app',
+  name: 'App',
   components: {
-    'container': container
+    HelloWorld
   }
 }
 </script>
 
-<style>
-body {
-  /* background-color: #EEFFFF; */
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
 }
 </style>
