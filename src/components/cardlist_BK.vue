557e3277d12b9b9487e0952a42a33e204a448760
Naoki Tsunoda <Tn04240709@icloud.com>
2020-11-25 10:42:12 +0900

    テスト

---
+++ src/components/cardlist_BK.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="cardlist-area">
+    <div class="cardlist" draggable="true" v-for="card in filteredCardList" :key="card.id" :style="getUrl(card.url)" v-on:click="$emit('openPicture', card.no)"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'cardlist',
+  props: ['filteredCardList'],
+  methods: {
+    getUrl: function (url) {
+      return { 'background-image': 'url("' + url + '")' }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.cardlist-area {
+  display: flex;
+  width: 850px;
+  flex-wrap: wrap;
+  margin-left: auto;
+  margin-right: auto;
+}
+.cardlist {
+  border-radius: 10px;
+  margin: 5px 10px;
+  width: 150px;
+  height: 200px;
+  text-align: center;
+  cursor: pointer;
+  background-size: 100% 100%;
+  box-shadow: 10px 10px 15px -10px;
+  transform-style: preserve-3d;
+  perspective: 600px;
+}
+.cardlist:hover {
+  animation-name: cardHoberAnimation;
+  animation-duration: 0.2s;
+  animation-timing-function: ease-out;
+  animation-fill-mode:forwards;
+  z-index: 1;
+}
+@keyframes cardHoberAnimation {
+  0% { transform: scale(1.0) ; }
+  20% { transform: scale(1.2) rotateX(-10deg) rotateY(5deg) rotateZ(0deg); }
+  80% { transform: scale(1.8) rotateX(15deg) rotateY(-10deg) rotateZ(5deg); }
+  100% {
+    transform: scale(2.0) ;
+    box-shadow: 20px 20px 15px -10px;
+  }
+}
+</style>
---
+++ src/components/headder.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="headder-area">
+    <h1 class="title">カード一覧</h1>
+    <!-- <div class="serchberTest"></div> -->
+    <!-- <div v-bind:class="['searchBtn', {serchBtnHidden: isSerchBtnHidden}]" v-on:click.prevent="$emit('openSearchOverlay')" v-on:touchend.prevent="$emit('openSearchOverlay')"></div> -->
+    <!-- <div v-bind:class="['searchBtn', {serchBtnHidden: isSerchBtnHidden}]" @click="clickHandler"></div> -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'headder',
+  props: ['isSerchBtnHidden'],
+  data: function () {
+    return {
+      testW: 0,
+      testH: 0
+    }
+  },
+  created: function () {
+    this.testH = window.innerHeight
+    this.testW = window.innerzWidth
+  // ]    this.testH = window.outerHeight
+  //     this.testW = window.outerWidth
+  }
+}
+</script>
+
+<style scoped>
+.headder-area {
+  width: 100%;
+  padding: 5px 0px 5px 0px;
+  margin-bottom: 10px;
+  position: relative;
+}
+
+.title {
+  /* width: 850px; */
+  width: 100%;
+  text-align: center;
+  margin-left: auto;
+  margin-right: auto;
+  border-bottom: solid 3px #C0C0C0;
+  font-family: Arial, "ヒラギノ角ゴ Pro W3", "Hiragino Kaku Gothic Pro", Osaka, メイリオ, Meiryo, "ＭＳ Ｐゴシック", "MS PGothic", sans-serif;
+}
+
+/* .searchBtn {
+  position: fixed;
+  top: 20px;
+  right: 100px;
+  height: 100px;
+  width: 100px;
+  border-radius: 50%;
+  text-align: center;
+  box-shadow: 0 0 3px 0 rgba(0,0,0,.12), 0 2px 3px 0 rgba(0,0,0,.22);
+  transition: .3s;
+  overflow: hidden;
+  background-image: url(../assets/search.jpg);
+  background-size: cover;
+  z-index: 1;
+}
+
+.searchBtn:hover {
+  box-shadow: 0 15px 30px -5px rgba(0,0,0,.15), 0 0 5px rgba(0,0,0,.1);
+  transform: translateY(-4px);
+  cursor: pointer;
+}
+
+.serchBtnHidden {
+  opacity: 0;
+} */
+
+.serchberTest {
+  /* background-color: #AAAAAA; */
+  width: 850px;
+  height: 70px;
+  margin-left: auto;
+  margin-right: auto;
+  border: solid 3px #AAAAAA;
+}
+</style>
