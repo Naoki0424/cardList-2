2467824ecd8fb7277e861260145f1cd7479938c2
Naoki Tsunoda <Tn04240709@icloud.com>
2020-11-28 13:31:40 +0900

    スマホ用の検索画面少し修正

--- src/components/overlay-search.vue
+++ src/components/overlay-search.vue
@@ -45,38 +45,6 @@ export default {
   align-items: center;
   justify-content: center;
 }
-.searchScreen {
-  width: 800px;
-  height: auto;
-  background-color: #FFFFFF;
-  border-radius: 10px;
-  animation: SlideIn 0.5s;
-}
-.searchTitle {
-  text-align: center;
-  font-family: Arial, "ヒラギノ角ゴ Pro W3", "Hiragino Kaku Gothic Pro", Osaka, メイリオ, Meiryo, "ＭＳ Ｐゴシック", "MS PGothic", sans-serif;
-  border-bottom: solid 3px #C0C0C0;
-}
-.serchOptionContainer {
-  display: grid;
-  grid-template-areas:
-  "a b c"
-  "a d e"
-  "f g h"
-  "f i j";
-  grid-template-columns: 20% 40% 40%;
-  grid-template-rows: 50px 50px 50px 50px;
-}
-.catergoryPack{grid-area: a; background-color: #EEEEEE; border-bottom: 1px solid; border-top: 1px solid; border-color: #CCCCCC;}
-.optionPackkp01{grid-area: b; border-top: 1px solid; border-color: #CCCCCC;}
-.optionPackkp02{grid-area: c; border-top: 1px solid; border-color: #CCCCCC;}
-.optionPackcp01{grid-area: d; border-bottom: 1px solid; border-color: #CCCCCC;}
-.optionPackkp03{grid-area: e; border-bottom: 1px solid; border-color: #CCCCCC;}
-.catergoryType{grid-area: f; background-color: #EEEEEE; border-bottom: 1px solid; border-color: #CCCCCC;}
-.optionType00{grid-area: g;}
-.optionType01{grid-area: h;}
-.optionType02{grid-area: i; border-bottom: 1px solid; border-color: #CCCCCC;}
-.optionType03{grid-area: j; border-bottom: 1px solid; border-color: #CCCCCC;}
 
 .buttonContainer {
   width: 100%;
@@ -89,4 +57,79 @@ export default {
   margin: 0 auto;
 }
 input{ font-size:16px; }
+
+/* スマホ想定 */
+@media only screen and (max-width: 599px) {
+  .searchScreen {
+    width: 400px;
+    height: auto;
+    background-color: #FFFFFF;
+    border-radius: 10px;
+    animation: SlideIn 0.5s;
+  }
+  .searchTitle {
+    text-align: center;
+    font-family: Arial, "ヒラギノ角ゴ Pro W3", "Hiragino Kaku Gothic Pro", Osaka, メイリオ, Meiryo, "ＭＳ Ｐゴシック", "MS PGothic", sans-serif;
+    border-bottom: solid 3px #C0C0C0;
+  }
+  .serchOptionContainer {
+    display: grid;
+    grid-template-areas:
+    "a b"
+    "a c"
+    "a d"
+    "a e"
+    "f g"
+    "f h"
+    "f i"
+    "f j";
+    grid-template-columns: 20% 80%;
+  }
+  .catergoryPack{grid-area: a; background-color: #EEEEEE; border-bottom: 1px solid #CCCCCC; border-top: 1px solid #CCCCCC;}
+  .optionPackkp01{grid-area: b; border-bottom: 1px solid #CCCCCC; border-top: 1px solid #CCCCCC;}
+  .optionPackkp02{grid-area: c; border-bottom: 1px solid #CCCCCC;}
+  .optionPackcp01{grid-area: d; border-bottom: 1px solid #CCCCCC;}
+  .optionPackkp03{grid-area: e; border-bottom: 1px solid #CCCCCC;}
+  .catergoryType{grid-area: f; background-color: #EEEEEE; border-bottom: 1px solid #CCCCCC;}
+  .optionType00{grid-area: g; border-bottom: 1px solid #CCCCCC;}
+  .optionType01{grid-area: h; border-bottom: 1px solid #CCCCCC;}
+  .optionType02{grid-area: i; border-bottom: 1px solid; border-color: #CCCCCC;}
+  .optionType03{grid-area: j; border-bottom: 1px solid; border-color: #CCCCCC;}
+}
+
+/* スマホ以外想定 */
+@media only screen and (min-width: 600px) {
+  .searchScreen {
+    width: 800px;
+    height: auto;
+    background-color: #FFFFFF;
+    border-radius: 10px;
+    animation: SlideIn 0.5s;
+  }
+  .searchTitle {
+    text-align: center;
+    font-family: Arial, "ヒラギノ角ゴ Pro W3", "Hiragino Kaku Gothic Pro", Osaka, メイリオ, Meiryo, "ＭＳ Ｐゴシック", "MS PGothic", sans-serif;
+    border-bottom: solid 3px #C0C0C0;
+  }
+  .serchOptionContainer {
+    display: grid;
+    grid-template-areas:
+    "a b c"
+    "a d e"
+    "f g h"
+    "f i j";
+    grid-template-columns: 20% 40% 40%;
+    grid-template-rows: 50px 50px 50px 50px;
+  }
+  .catergoryPack{grid-area: a; background-color: #EEEEEE; border-bottom: 1px solid; border-top: 1px solid; border-color: #CCCCCC;}
+  .optionPackkp01{grid-area: b; border-top: 1px solid; border-color: #CCCCCC;}
+  .optionPackkp02{grid-area: c; border-top: 1px solid; border-color: #CCCCCC;}
+  .optionPackcp01{grid-area: d; border-bottom: 1px solid; border-color: #CCCCCC;}
+  .optionPackkp03{grid-area: e; border-bottom: 1px solid; border-color: #CCCCCC;}
+  .catergoryType{grid-area: f; background-color: #EEEEEE; border-bottom: 1px solid; border-color: #CCCCCC;}
+  .optionType00{grid-area: g;}
+  .optionType01{grid-area: h;}
+  .optionType02{grid-area: i; border-bottom: 1px solid; border-color: #CCCCCC;}
+  .optionType03{grid-area: j; border-bottom: 1px solid; border-color: #CCCCCC;}
+}
 </style>
